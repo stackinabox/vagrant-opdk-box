c521b1d73405794b47440409e371c765aa11af76
Tim Pouyer <tpouyer@us.ibm.com>
2016-06-02 22:10:10 -0400

    working!!!

--- scripts/docker/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
-apt-get update
-apt-get install -y docker-engine
--- scripts/docker/mirror.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cat > /etc/default/docker <<EOF
-DOCKER_OPTS="--dns 8.8.8.8 --dns 8.8.4.4 --userland-proxy=false --registry-mirror=http://192.168.27.100:4000 --insecure-registry=192.168.27.100:4000"
-EOF
-service docker restart
